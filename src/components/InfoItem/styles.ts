45f30a9ce409181edd836ae84c0532cb6a942973
Luan Cordeiro <luam.lc19@gmail.com>
2023-02-06 21:49:20 -0300

    style: finish style of InfoItem

--- src/components/InfoItem/styles.ts
+++ src/components/InfoItem/styles.ts
@@ -1,3 +1,16 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    margin: 20px 0;
+`;
+
+export const Label = styled.div`
+    font-size: 15px;
+    color: #6A7D8B;
+`;
+
+export const Value = styled.div`
+    color: #101C40;
+    font-weight: bold;
+    font-size: 37px;
+`;
