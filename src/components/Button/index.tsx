88564e91b13e5e1a50cacc4d43f366260e318d16
Luan Cordeiro <luam.lc19@gmail.com>
2023-02-07 13:39:23 -0300

    style: create button structure, no functional

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,19 @@
 import * as C from './styles';
 
-export const Button = () => {
+type Props = {
+    label: string;
+    icon?: string;
+}
+
+export const Button = ({ label, icon }: Props) => {
     return (
         <C.Container>
-            ...
+            {icon &&
+                <C.IconArea>
+                    <C.Icon src={icon} />
+                </C.IconArea>
+            }
+            <C.Label>{label}</C.Label>
         </C.Container>
     )
 }
