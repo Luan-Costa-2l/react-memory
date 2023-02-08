fab654163385b1e22b0431dcbc2bcb99d38cf85f
Luan Cordeiro <luam.lc19@gmail.com>
2023-02-08 20:40:30 -0300

    feat: implement reset and create grid function

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,37 @@ const App = () => {
   const [timeElapsed, setTimeElapsed] = useState<number>(0);
   const [gridItems, setGridItems] = useState<GridItemType[]>([]);
 
+  const resetAndCreateGrid = () => {
+    // passo 1 - resetar o jogo
+    setShownCount(0);
+    setMoveCount(0);
+    setTimeElapsed(0);
+
+    // passo 2 - criar o grid e começar o jogo
+    // 2.1 criar grid vazio
+    let newGrid: GridItemType[] = [];
+    for (let i = 0; i < (items.length * 2); i++) newGrid.push({
+      item: null, permanentShown: false, shown: false
+    });
+
+    // 2.3 - preencher o grid
+    for (let w = 0; w < 2; w++) {
+      for (let i = 0; i < items.length; i++) {
+        let pos = -1;
+        while (pos < 0 || gridItems[pos].item !== null) {
+          pos = Math.floor(Math.random() * (items.length * 2))
+        }
+        newGrid[pos].item = i;
+      }
+    }
+
+    // 2.3 - jogar no state
+    setGridItems(newGrid);
+
+    // passo 3 começar o jgo
+    setPlaying(true);
+  }
+
   return (
     <C.Container>
       <C.Info>
