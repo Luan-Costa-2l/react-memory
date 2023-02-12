efcc2bde38214cd89c63a5f50e1c0ec71814d04b
Luan Cordeiro <luam.lc19@gmail.com>
2023-02-12 19:55:59 -0300

    feat: import GridItemType, items, b7svg

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -1,4 +1,7 @@
 import * as C from './styles';
+import { GridItemType } from '../../types/GridItemType';
+import { items } from '../../data/items';
+import b7svg from '../../svgs/b7.svg';
 
 export const GridItem = () => {
     return (
