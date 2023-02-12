e0931aedb4351b199ccc7f47c82fbc7940fa66e1
Luan Cordeiro <luam.lc19@gmail.com>
2023-02-12 19:59:26 -0300

    style: create styles of GridItem Component

--- src/components/GridItem/styles.ts
+++ src/components/GridItem/styles.ts
@@ -1,3 +1,24 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+type ContainerProps = {
+    shownBackground: boolean;
+}
+
+type IconProps = {
+    opacity?: number;
+}
+
+export const Container = styled.div<ContainerProps>`
+    height: 100px;
+    background-color: ${props => props.shownBackground ? '#1550FF' : '#E2E3E3'};
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Icon = styled.img<IconProps>`
+    width: 40px;
+    height: 40px;
+    opacity: ${props => props.opacity ?? 1};
+`;
