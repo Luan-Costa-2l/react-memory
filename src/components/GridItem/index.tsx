5f21d9fb4aaebd7eddc8df54a24c36081ebff2e9
Luan Cordeiro <luam.lc19@gmail.com>
2023-02-12 19:59:57 -0300

    feat: create GridItem component

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -3,10 +3,20 @@ import { GridItemType } from '../../types/GridItemType';
 import { items } from '../../data/items';
 import b7svg from '../../svgs/b7.svg';
 
-export const GridItem = () => {
+type Props = {
+    item: GridItemType;
+    onClick: () => void;
+}
+
+export const GridItem = ({ item, onClick }: Props) => {
     return (
-        <C.Container>
-            ...
+        <C.Container shownBackground={item.permanentShown || item.shown} onClick={onClick}>
+            {item.item !== null && (item.permanentShown || item.shown) &&
+                <C.Icon src={items[item.item].icon} alt={items[item.item].name} />
+            }
+            {!item.permanentShown && !item.shown &&
+                <C.Icon src={b7svg} opacity={0.1} />
+            }
         </C.Container>
     );
 }
